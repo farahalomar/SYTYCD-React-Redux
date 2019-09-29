eeff73e7d58b0a131d62ac3108ad821836490a4c
Farah AlOmar <farahjalomar@gmail.com>
2019-09-29 15:17:19 +0300

    farah

--- src/store/reducers/authors.js
+++ src/store/reducers/authors.js
@@ -8,11 +8,11 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_AUTHORS:
+    case actionTypes.FETCH_AUTHORS:
       return {
         ...state,
-        authors: state.authors.push(action.payload),
-        filteredAuthors: state.filteredAuthors.concat(action.payload),
+        authors: action.payload,
+        filteredAuthors: action.payload,
         loading: false
       };
 
